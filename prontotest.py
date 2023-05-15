71211c7fd292ba509ab3b640ca713b7fa3b3dc3b
Isabel Frade <isabelf@instemlss.co.uk>
2023-05-15 16:37:51 -0400

    working pronto file

--- prontotest.py
+++ prontotest.py
@@ -1,29 +1,43 @@
+#This is a program to get all of the relationships of an ontology
+#Important! This program was written for python3.6.8. 
+#the pronto version MUST be 2.4.7 !!!!
+
+"""
+Things to address:
+Does it work with .obo files?
+"""
 import itertools
 import os
-import unittest
-import warnings
-
 import pronto
 from pronto.relationship import Relationship, RelationshipData
 def prontoRelations(ontoLibrary):
-    ontoL = pronto.Ontology.from_obo_library(ontoLibrary)
-    print(len(ontoL.terms()))
+    ontoL = pronto.Ontology.from_obo_library(ontoLibrary) #get ontology
+    print(len(ontoL.terms())) #number of terms in ontology
     with open('test-pronto.txt', 'w', encoding='utf-8') as file:
         for term in sorted(ontoL.terms()):
             #print(term)
-            file.write(f'{term}\n')
-            relationshipkeys = sorted(term.relationships.keys())
+            file.write(f'{term}\n') #write the term id & name
+            try:
+                relationshipkeys = sorted(term.relationships.keys())
+            except KeyError as e:
+                pass
+             #^this gets the relationships itself,but not the term connected to it
             file.write(f'Relationships:\n')
             for key in relationshipkeys:
                 keyID = key.id
                 keyName= key.name
-                relations = term.relationships[ontoL.get_relationship(keyID)]
-                file.write(f"{keyName}:  {relations}\n")
+                try:
+                    relations = term.relationships[ontoL.get_relationship(keyID)]
+                    file.write(f"{keyName}: {relations}\n")
+                except KeyError as e:
+                    pass
             children: pronto.TermSet = term.subclasses(with_self=True).to_set()
             file.write(f'children: {children}\n')
             parents: pronto.TermSet = term.superclasses(with_self=True).to_set()
             file.write(f'parents: {parents}\n')
+"""
     with open('test-pronto-relationships.txt', 'w', encoding='utf-8') as file2:
         for relation in ontoL.relationships():
             file2.write(f'{relation}\n')
-prontoRelations("lepao.owl")
+"""
+prontoRelations("lepao.obo")
